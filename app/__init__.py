72535315af6ecf75aeef61c00fad1ceac5a7b923
kcrong <kcrong@kim82536.pe.kr>
2015-12-17 11:13:14 +0900

    before edit profile img to round

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
+
+
+@app.route('/css/<path:filename>')
+def css_static(filename):
+    return send_from_directory(app.root_path + '/static/css/', filename)
+
+
+@app.route('/js/<path:filename>')
+def js_static(filename):
+    return send_from_directory(app.root_path + '/static/js/', filename)
+
+
+@app.route('/img/<path:filename>')
+def img_static(filename):
+    return send_from_directory(app.root_path + '/static/img/', filename)
+
+
+@app.route('/font/<path:filename>')
+def font_static(filename):
+    return send_from_directory(app.root_path + '/static/font', filename)
 
 
 def create_app():
--- manage.py
+++ manage.py
@@ -6,14 +6,15 @@ from flask.ext.script import Manager
 
 from app import create_app
 
-
 app = create_app()
 # app.wsgi_app = ProxyFix(app.wsgi_app)
 manager = Manager(app)
 
+
 @manager.command
 def run():
     app.run()
+
 
 if __name__ == "__main__":
     manager.run()
